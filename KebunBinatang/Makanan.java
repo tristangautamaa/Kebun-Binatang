c9cf5e832192881b0a0be779bd3eb482231e76ef
JonathanCahyo <jonathancwp6605@gmail.com>
2024-03-09 09:45:34 +0700

    Menambahkan p ubah
    Menggunakan system out println

--- KebunBinatang/Makanan.java
+++ KebunBinatang/Makanan.java
@@ -3,5 +3,7 @@ package KebunBinatang;
 public class Makanan {
     public static void main(String[] args) {
         System.out.println("test joe");
+        System.out.println("p ubah");
+
     }
 }
