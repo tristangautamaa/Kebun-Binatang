9c477c989f3956e1738a19f0b4a7e36e6571a2fd
Fernando <fernandogunawan7@gmail.com>
2024-03-12 17:16:52 +0700

    update halo babi
    menambahkan print text halo babi

--- KebunBinatang/main.java
+++ KebunBinatang/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Selamat siang");
         System.out.println("halo wilson");
+        System.out.println("Halo babi!");
     }
 }
