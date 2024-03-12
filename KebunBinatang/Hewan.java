89bfc2cb727b48a0fc60c43983c9512d25c3dcc3
RamaHarrison <Ramaliu2013@gmail.com>
2024-03-12 17:19:57 +0700

    Text nando asu
    nambahin untuk ...

--- KebunBinatang/Hewan.java
+++ KebunBinatang/Hewan.java
@@ -3,5 +3,6 @@ package KebunBinatang;
 public class Hewan {
     public static void main(String[] args) {
         System.out.println("hari pertama test");
+        System.out.println("Nando ASu");
     }
 }
