7dd50a7b10715fb3d66474b1041e6b36f5d81a2a
wilsonliyanto3 <141407742+wilsonliyanto3@users.noreply.github.com>
2024-03-14 15:40:16 +0700

    Class Hewan
    attribut dan constructor

--- KebunBinatang/Hewan.java
+++ KebunBinatang/Hewan.java
@@ -1,6 +1,25 @@
 package KebunBinatang;
 
 public abstract class Hewan {
+    private String nama;
+    private String makanan;
+    private String habitat;
+
+    public Hewan (String nama, String makanan, String habitat){
+        this.nama = nama;
+        this.makanan = makanan;
+        this.habitat = habitat;
+    }
+    public String nama() {
+        return nama;
+    }
+    public String makanan() {
+        return makanan;
+    }
+    public String habitat() {
+        return habitat;
+    }
+
     // ISINYA ATRIBUT nama, makanan, habitat dan constructornya sehingga di
     // Karnivora dan Hebivora CUKUP manggil pake SUPER
     // abstract class ini isinya yang dikit itu
