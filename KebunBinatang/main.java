897be8005db66a74423b4e78358cb6975078a13d
Tristan Nathanael Gautama <tristangautamaa@gmail.com>
2024-03-14 15:06:34 +0700

    Menambahkan hewan 3 elang

--- KebunBinatang/main.java
+++ KebunBinatang/main.java
@@ -4,6 +4,7 @@ public class main {
     public static void main(String[] args) {
         Hewan hewan1 = new Hewan("Harimau", "Daging", "Hutan");
         Hewan hewan2 = new Hewan("Kambing", "Rumput", "Ladang");
+        Hewan hewan3 = new Hewan("Elang", "Daging", "Gunung");
         Pengunjung pengunjung1 = new pengjunjung("Jonathan", "17");
         Pengunjung pengunjung2 = new pengunjung("Fernando", "18");
 
