7828a5eb43c7be3c5c2b11ecd1625ffb9af5307b
RamaHarrison <Ramaliu2013@gmail.com>
2024-03-14 15:50:02 +0700

    Menambahkan class harbivora
    ...

--- KebunBinatang/Herbivora.java
+++ KebunBinatang/Herbivora.java
@@ -1,6 +1,15 @@
 package KebunBinatang;
 
 public class Herbivora extends Hewan {
+    Public Herbivora (String nama, String makanan, String habitat) {
+        super(nama, makanan, habitat);
+        }
+
+        @Override
+        void aktivitas(){
+            System.out.println( nama + " sedang memakan " + makanan + ".");
+        }
+
     // Isinya SUPER dari --> name, makanan, habitat
     // Mengandung override sebuah method yang tidak ada di abstract
     // Override itu keluarnya bareng tapi outputnya berbeda
