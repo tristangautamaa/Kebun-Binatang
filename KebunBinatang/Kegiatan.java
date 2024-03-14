8ff1835cf84fca78914a9234244130f20c4f6c70
Tristan Nathanael Gautama <tristangautamaa@gmail.com>
2024-03-14 08:39:11 +0700

    Mengerjakan class Kegiatan
    menciptakan atribut dan constructor dari name dan membuat 1 method bernama checkCondition()

--- KebunBinatang/Kegiatan.java
+++ KebunBinatang/Kegiatan.java
@@ -1,5 +1,15 @@
 package KebunBinatang;
 
 public class Kegiatan {
-    // Do Code Here
+    // Attribute to Astronaut Class
+    String name;
+
+    // Constructor to initialize the attributes
+    public Kegiatan(String name) {
+        this.name = name;
+    }
+
+    public void checkCondition() {
+        System.out.println(name + " is busy playing.");
+    }
 }
