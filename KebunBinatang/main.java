e5c305339aeaad20615b7c1f28428ce226e3b15f
Tristan Nathanael Gautama <tristangautamaa@gmail.com>
2024-03-15 12:56:05 +0700

    MAIN nya ada ; ketinggalan tipis

--- KebunBinatang/main.java
+++ KebunBinatang/main.java
@@ -7,7 +7,7 @@ public class main {
         Hewan hewan3 = new Hewan("Elang", "Daging", "Gunung");
         Pengunjung pengunjung1 = new Pengunjung("Jonathan", 17, "Jakarta");
         Pengunjung pengunjung2 = new Pengunjung("Fernando", 18, "Pontianak");
-        
+
         hewan1.info();
         hewan2.info();
         hewan3.info();
@@ -17,16 +17,16 @@ public class main {
         Hewan hewan1 = new Hewan("Harimau", "Daging", "Hutan");
         hewan1.setDeskripsi("Mamalia besar dengan gigi tajam dan loreng oranye hitam.");
         hewan1.setSuara("roar");
-        hewan1.setKegiatan("makan")
-    
+        hewan1.setKegiatan("makan");
+
         Hewan hewan2 = new Hewan("Kambing", "Rumput", "Ladang");
-        hewan2.setDeskripsi("Hewan herbivora yang memakan dedaunan")
+        hewan2.setDeskripsi("Hewan herbivora yang memakan dedaunan");
         hewan2.setSuara("mbeee");
-        hewan2.setKegiatan("tidur")
-    
+        hewan2.setKegiatan("tidur");
+
         Hewan hewan3 = new Hewan("Elang", "Daging", "Gunung");
         hewan3.setDeskripsi("Hewan berdarah panas, mempunyai sayap dan tubuh yang diselubungi bulu pelepah");
         hewan3.setSuara("skriaa");
-        hewan3.setKegiatan("bertengger")
+        hewan3.setKegiatan("bertengger");
     }
 }
