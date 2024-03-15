db3661665fc0520913340ba7aa5a21d0e480dc8f
Tristan Nathanael Gautama <tristangautamaa@gmail.com>
2024-03-15 11:29:24 +0700

    FINALISASI MAIN
    ini main class utama yang akan dipake mulai sekarang

--- KebunBinatang/main.java
+++ KebunBinatang/main.java
@@ -5,12 +5,26 @@ public class main {
         Hewan hewan1 = new Hewan("Harimau", "Daging", "Hutan");
         Hewan hewan2 = new Hewan("Kambing", "Rumput", "Ladang");
         Hewan hewan3 = new Hewan("Elang", "Daging", "Gunung");
-        Pengunjung pengunjung1 = new pengunjung("Jonathan", "17");
-        Pengunjung pengunjung2 = new pengunjung("Fernando", "18");
-
-        // to be continued
+        Pengunjung pengunjung1 = new Pengunjung("Jonathan", "17", "Jakarta");
+        Pengunjung pengunjung2 = new Pengunjung("Fernando", "18", "Pontianak");
+        
         pengunjung1.info();
         pengunjung2.info();
         pengunjung1.info();
+
+        Hewan hewan1 = new Hewan("Harimau", "Daging", "Hutan");
+        hewan1.setDeskripsi("Mamalia besar dengan gigi tajam dan loreng oranye hitam.");
+        hewan1.setSuara("roar");
+        hewan1.setKegiatan("makan")
+    
+        Hewan hewan2 = new Hewan("Kambing", "Rumput", "Ladang");
+        hewan2.setDeskripsi("Hewan herbivora yang memakan dedaunan")
+        hewan2.setSuara("mbeee");
+        hewan2.setKegiatan("tidur")
+    
+        Hewan hewan3 = new Hewan("Elang", "Daging", "Gunung");
+        hewan3.setDeskripsi("Hewan berdarah panas, mempunyai sayap dan tubuh yang diselubungi bulu pelepah");
+        hewan3.setSuara("skriaa");
+        hewan3.setKegiatan("bertengger")
     }
 }
