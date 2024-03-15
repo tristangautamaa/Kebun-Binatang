c3d2524655080ab4738eee9246035b38bf5d6a0c
RamaHarrison <Ramaliu2013@gmail.com>
2024-03-15 13:18:08 +0700

    Testing herbivora
    nah ini masih blom pasti, soalnya belum ada gambaran yang jelas dengan outputnya...

--- KebunBinatang/Herbivora.java
+++ KebunBinatang/Herbivora.java
@@ -1,17 +1,24 @@
 package KebunBinatang;
-
 public class Herbivora extends Hewan {
-    Public Herbivora (String nama, String makanan, String habitat) {
-        super(nama, makanan, habitat);
-        }
 
-        @Override
-        void aktivitas(){
-            System.out.println( nama + " sedang memakan " + makanan + ".");
-        }
+    public Herbivora(String nama, String habitat) {
+        super(nama, "Rumput", habitat);
+    }
+
+    @Override
+    
+    // nah ini masih blom pasti, soalnya belum ada gambaran 
+    // yang jelas dengan outputnya
+    // soal kegiatan atau info..
+    public void info() { 
+        System.out.println("Nama: " + nama);
+        System.out.println("Makanan: " + makanan);
+        System.out.println("Habitat: " + habitat);
+    }
+}
 
     // Isinya SUPER dari --> name, makanan, habitat
     // Mengandung override sebuah method yang tidak ada di abstract
     // Override itu keluarnya bareng tapi outputnya berbeda
     // Mengandung hewan sedang melakukan apa
-}
+
