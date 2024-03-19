6117c46e8fedafdbaaae67b604d620578d72e6ad
JonathanCahyo <jonathancwp6605@gmail.com>
2024-03-19 15:34:52 +0700

    Menambahkan suara, kegiatan, deskripsi

--- KebunBinatang/Karnivora.java
+++ KebunBinatang/Karnivora.java
@@ -1,18 +1,36 @@
 package KebunBinatang;
 
 public class Karnivora extends Herbivora {
-    // TAMBAHKAN KEGIATAN, DESKRIPSI, SUARA
-    public Karnivora(String name, String makanan, String habitat) {
-        super(name, makanan, habitat);
-    }
+    
+    private String deskripsi;
+    private String suara;
+    private String kegiatan;
 
+    public Karnivora(String nama, String makanan, String habitat, String deskripsi, String suara, String kegiatan) {
+        super(nama, makanan, habitat); 
+        this.deskripsi = deskripsi;
+        this.suara = suara;
+        this.kegiatan = kegiatan;
+    }
+    
     @Override
     public void info() {
-        System.out.println("Nama Hewan: " + namahewan);
-        System.out.println("Makanan: " + makanan);
-        System.out.println("Habitat: " + habitat);
+        super.info(); 
         System.out.println("Deskripsi: " + deskripsi);
         System.out.println("Suara: " + suara);
         System.out.println("Kegiatan: " + kegiatan);
+        System.out.println();
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public void setSuara(String suara) {
+        this.suara = suara;
+    }
+
+    public void setKegiatan(String kegiatan) {
+        this.kegiatan = kegiatan;
     }
 }
