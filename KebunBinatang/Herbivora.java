9cd9f823f3a5dcfbd4ee5a67f8dfdd247bdb29f8
RamaHarrison <Ramaliu2013@gmail.com>
2024-03-19 15:41:50 +0700

    deskkripsi, suara, kegiatan
    tambahan

--- KebunBinatang/Herbivora.java
+++ KebunBinatang/Herbivora.java
@@ -2,26 +2,39 @@ package KebunBinatang;
 
 public class Herbivora extends Hewan {
 
-    // TAMBAHKAN KEGIATAN, DESKRIPSI, SUARA
-    public Herbivora(String nama, String makanan, String habitat) {
+    private String deskripsi;
+    private String suara;
+    private String kegiatan;
+
+    public Herbivora(String nama, String makanan, String habitat, String deskripsi, String suara, String kegiatan) {
         super(nama, makanan, habitat);
+        this.deskripsi = deskripsi;
+        this.suara = suara;
+        this.kegiatan = kegiatan;
     }
 
     @Override
-
-    // nah ini masih blom pasti, soalnya belum ada gambaran
-    // yang jelas dengan outputnya
-    // soal kegiatan atau info..
     public void info() {
-        System.out.println("Nama Hewan: " + namahewan);
-        System.out.println("Makanan: " + makanan);
-        System.out.println("Habitat: " + habitat);
+        super.info(); 
         System.out.println("Deskripsi: " + deskripsi);
         System.out.println("Suara: " + suara);
         System.out.println("Kegiatan: " + kegiatan);
+        System.out.println();
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public void setSuara(String suara) {
+        this.suara = suara;
+    }
+
+    public void setKegiatan(String kegiatan) {
+        this.kegiatan = kegiatan;
     }
 }
-
+// masukkan deskripsi, suara, kegiatan
 // Isinya SUPER dari --> name, makanan, habitat
 // Mengandung override sebuah method yang tidak ada di abstract
 // Override itu keluarnya bareng tapi outputnya berbeda
